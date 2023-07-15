c3c4e55f7ea60197db9b0285c310bef5659ef1e8
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-15 01:20:07 -0300

    arrancando angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../components/login.component'; // Importa el LoginComponent
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent, // Agrega el LoginComponent en las declaraciones
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ client/src/components/login.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    template: '<h1>Hola Mundodddd</h1>',
+})
+export class LoginComponent {}
