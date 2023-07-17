98bf58a84e6ebf151dadd572af099903cba7cf84
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-17 03:09:45 -0300

    alerts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent, LoginComponent, RegisterComponent],
-    imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot({
+            positionClass: 'toast-bottom-center', // Ajusta la posición de la notificación
+            closeButton: true, // Muestra el botón de cerrar en la notificación
+        }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-register',
@@ -12,7 +14,11 @@ export class RegisterComponent {
     email: string = '';
     password: string = '';
 
-    constructor(private http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private toastr: ToastrService,
+        private router: Router
+    ) {}
 
     registerUser() {
         const newUser = {
@@ -24,14 +30,27 @@ export class RegisterComponent {
         };
 
         this.http.post('http://localhost:3000/register', newUser).subscribe(
-            (response) => {
+            (response: any) => {
                 console.log('Usuario registrado exitosamente', response);
+
+                const toastrConfig: Partial<IndividualConfig> = {
+                    timeOut: 2000, // Duración de 2 segundos (en milisegundos)
+                };
+                this.toastr.success(
+                    'User registered successfully',
+                    'Success',
+                    toastrConfig
+                );
+
                 // Restablecer los valores del formulario
                 this.firstname = '';
                 this.lastname = '';
                 this.username = '';
                 this.email = '';
                 this.password = '';
+
+                // Redirigir al componente de inicio de sesión
+                this.router.navigate(['/login']);
             },
             (error) => {
                 console.error('Error al registrar el usuario', error);
