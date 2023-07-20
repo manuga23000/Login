03752c11e87b239def8c8ffdf70fa9aebc76717a
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-20 04:38:58 -0300

    next act

---
+++ client/my-next-app/pages/api/login.tsx
@@ -0,0 +1,6 @@
+// frontend/pages/login.tsx
+const Login: React.FC = () => {
+  return <div>Login Page</div>;
+};
+
+export default Login;
---
+++ client/my-next-app/pages/api/register.tsx
@@ -0,0 +1,6 @@
+// frontend/pages/register.tsx
+const Register: React.FC = () => {
+  return <div>Register Page</div>;
+};
+
+export default Register;
