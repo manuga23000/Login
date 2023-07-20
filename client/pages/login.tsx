1f505c22fc6fe29afaaf1bdff2bbd92bb729629e
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-20 05:46:05 -0300

    inicializando next funcionando

---
+++ client/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            {/* Aquí agregarías el formulario y la lógica para el inicio de sesión */}
+        </div>
+    );
+};
+
+export default Login;
---
+++ client/pages/register.tsx
@@ -0,0 +1,13 @@
+// /pages/register.tsx
+import React from 'react';
+
+const Register: React.FC = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            {/* Aquí agregarías el formulario y la lógica para el registro */}
+        </div>
+    );
+};
+
+export default Register;
