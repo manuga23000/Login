f544b93643426f47e8212d627a118c9eeceebfde
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-28 03:17:55 -0300

    Update routes.ts

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -63,5 +63,46 @@ router.post('/register', async (req: Request, res: Response) => {
 });
 
 // Ruta para iniciar sesión
+router.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { usernameOrEmail, password } = req.body;
+
+        // Buscar al usuario en la base de datos por nombre de usuario o correo electrónico
+        const user = await User.findOne({
+            $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
+        });
+        if (!user) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+
+        // Verificar si la contraseña proporcionada coincide con la almacenada en la base de datos
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Contraseña incorrecta' });
+        }
+
+        // Generar un token JWT con la información del usuario y la clave secreta
+        const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            'secreto'
+        ); // Reemplaza 'secreto' con tu propia clave secreta
+
+        res.status(200).json({
+            message: 'Inicio de sesión exitoso',
+            token,
+            user: {
+                _id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                username: user.username,
+                email: user.email,
+                role: user.role,
+            },
+        });
+    } catch (error) {
+        console.error('Error al iniciar sesión', error);
+        res.status(500).json({ error: 'Error al iniciar sesión' });
+    }
+});
 
 export default router;
