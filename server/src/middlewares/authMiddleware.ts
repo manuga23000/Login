c516fc8f827ae90c25948f65e50bbc4cbca80d98
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-30 23:43:37 -0300

    Update authMiddleware.ts

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -15,25 +15,17 @@ declare global {
 }
 
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    // Verificar si el usuario tiene el token de autenticación
     const token = req.headers.authorization;
     if (!token) {
         return res.status(401).json({ error: 'Acceso no autorizado' });
     }
 
     try {
-        // Verificar y decodificar el token
         const decodedToken: any = jwt.verify(token, 'secreto'); // Reemplaza 'secreto' con tu propia clave secreta
-
-        // Verificar si el usuario es administrador
         if (decodedToken.role !== 'admin') {
             return res.status(403).json({ error: 'Acceso no autorizado' });
         }
-
-        // Asignar el token decodificado al objeto 'user' de la solicitud
         req.user = decodedToken;
-
-        // El usuario es administrador, continúa con la siguiente función de middleware o controlador
         next();
     } catch (error) {
         console.error('Error al verificar el token', error);
